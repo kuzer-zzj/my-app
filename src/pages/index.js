1513a8ec4bae7f28268ee8d2cda85fac86843db7
zzj <156321781@qq.com>
2022-03-18 00:17:41 +0800

    记事本框框

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Edit from "./Home/components/Edit"
+import List from "./Home/components/List"
+import './index.css'
+
+
+const Home = () => {
+    return <div className="app">
+        <Edit></Edit>
+        <List></List>
+    </div>
+}
+
+export default Home
